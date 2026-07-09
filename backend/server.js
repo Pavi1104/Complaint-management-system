b919f771513394a7ef8ff2c294e974a4364b9213
Pavithra Lakshmi <pavithralakshmisasikumar@gmail.com>
2026-07-09 21:36:02 +0530

    Fix MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -33,8 +33,12 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(frontendPath, "index.html"));
+});
+
 /* MongoDB */
-const mongoUrl = process.env.MONGO_URL || "mongodb+srv://pavithralakshmisasikumar_db_user:n8oa8tpG86AuNSJa@cluster0.t3u74fa.mongodb.net/complaint_db";
+const mongoUrl = process.env.MONGO_URL;
 mongoose.connect(mongoUrl)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
